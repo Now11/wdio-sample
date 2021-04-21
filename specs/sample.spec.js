c4b5dcff596ebb9c3c2e796ebf6b86642fda91c1
Denys Kuznietsov <denys.kyznietsov@binary-studio.com>
2021-04-21 19:17:01 +0300

    init first

---
+++ specs/sample.spec.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const rndNumber = () => Math.floor(Math.random() * (100000 - 1) + 1).toString();
+
+describe('Ragistration:', function () {
+  it('Should register user', async function () {
+    const uuid = rndNumber();
+    const usernameField = await $('[placeholder="Username"]');
+    const emailField = await $('[placeholder="Email"]');
+    const passwordField = await $('[placeholder="Password"]');
+    const signUpButton = await $('[type="submit"]');
+
+    const title = await $('li a.ng-binding');
+
+    await browser.url('https://demo.realworld.io/#/register');
+    await browser.pause(3000);
+
+    await usernameField.setValue(`test+${uuid}`);
+    await emailField.setValue(`test+${uuid}@gmail.com`);
+    await passwordField.setValue('Pa55word');
+    await signUpButton.click();
+    await browser.pause(3000);
+
+    const profileName = await title.getText();
+
+    expect(profileName).to.eql(`test+${uuid}`);
+  });
+});
