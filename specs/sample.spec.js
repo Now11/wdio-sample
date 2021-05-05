fa0cd929f1915c3faad86a03c0f3de8cf26a0fc6
Denys Kuznietsov <denys.kyznietsov@binary-studio.com>
2021-05-05 15:06:27 +0300

    init

--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -1,27 +1,15 @@
 const { expect } = require('chai');
-const rndNumber = () => Math.floor(Math.random() * (100000 - 1) + 1).toString();
 
 describe('Ragistration:', function () {
   it('Should register user', async function () {
-    const uuid = rndNumber();
-    const usernameField = await $('[placeholder="Username"]');
-    const emailField = await $('[placeholder="Email"]');
-    const passwordField = await $('[placeholder="Password"]');
-    const signUpButton = await $('[type="submit"]');
+    await browser.url('https://webdriver.io');
 
-    const title = await $('li a.ng-binding');
+    const title = await $('.hero__subtitle');
+    await title.waitForDisplayed();
 
-    await browser.url('https://demo.realworld.io/#/register');
-    await browser.pause(3000);
-
-    await usernameField.setValue(`test+${uuid}`);
-    await emailField.setValue(`test+${uuid}@gmail.com`);
-    await passwordField.setValue('Pa55word');
-    await signUpButton.click();
-    await browser.pause(3000);
-
-    const profileName = await title.getText();
-
-    expect(profileName).to.eql(`test+${uuid}`);
+    const text = await title.getText();
+    expect(text).to.be.eql(
+      'Next-gen browser and mobile automation test framework for Node.js',
+    );
   });
 });
